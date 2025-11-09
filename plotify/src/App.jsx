3fd66c3f999de9b6abe815ee01093172796000f6
Issac Enrique Cervantes <enriquec@calpoly.edu>
2025-11-09 14:20:08 -0800

    Added screen routing with nav bar

--- plotify/src/App.jsx
+++ plotify/src/App.jsx
@@ -1,12 +1,29 @@
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router";
+import Home from "./components_screens/Home";
+import Events from "./components_screens/Events";
+import Messages from "./components_screens/Messages";
+import NavBar from "./components_screens/components/NavBar";
+
 
 function App() {
-
   return (
     <>
-      <h1 className='text-black font-bold'>Plotify</h1>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        
+        <Route path="/home" element={<Home />}/>
+        
+        <Route path="/events" element={<Events />}/>
+        
+        <Route path="/messages" element={<Messages />}/>
+                
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+        
+      </Routes>
+      <NavBar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ plotify/src/components_screens/components/NavBar.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router";
+
+export default function Home() {
+  return (
+    <nav className="min-h-full min-w-full">
+      <ul className="flex min-w-full min-h-full">
+        <li className="flex flex-grow items-center justify-center">
+          <Link to={"/"} className="flex flex-col text-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 640"
+              className="h-8"
+            >
+              {/* <!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
+              <path d="M341.8 72.6C329.5 61.2 310.5 61.2 298.3 72.6L74.3 280.6C64.7 289.6 61.5 303.5 66.3 315.7C71.1 327.9 82.8 336 96 336L112 336L112 512C112 547.3 140.7 576 176 576L464 576C499.3 576 528 547.3 528 512L528 336L544 336C557.2 336 569 327.9 573.8 315.7C578.6 303.5 575.4 289.5 565.8 280.6L341.8 72.6zM304 384L336 384C362.5 384 384 405.5 384 432L384 528L256 528L256 432C256 405.5 277.5 384 304 384z" />
+            </svg>
+            Home
+          </Link>
+        </li>
+        <li className="flex flex-grow items-center justify-center">
+          <Link to={"/events"} className="flex flex-col text-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 640"
+              className="h-8"
+            >
+              {/* <!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
+              <path d="M224 64C206.3 64 192 78.3 192 96L192 128L160 128C124.7 128 96 156.7 96 192L96 240L544 240L544 192C544 156.7 515.3 128 480 128L448 128L448 96C448 78.3 433.7 64 416 64C398.3 64 384 78.3 384 96L384 128L256 128L256 96C256 78.3 241.7 64 224 64zM96 288L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 288L96 288z" />
+            </svg>
+            Events
+          </Link>
+        </li>
+        <li className="flex flex-grow items-center justify-center">
+          <Link to={"/messages"} className="flex flex-col text-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8"
+              viewBox="0 0 640 640"
+            >
+              {/* <!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
+              <path d="M64 416L64 192C64 139 107 96 160 96L480 96C533 96 576 139 576 192L576 416C576 469 533 512 480 512L360 512C354.8 512 349.8 513.7 345.6 516.8L230.4 603.2C226.2 606.3 221.2 608 216 608C202.7 608 192 597.3 192 584L192 512L160 512C107 512 64 469 64 416z" />
+            </svg>
+            Messages
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
