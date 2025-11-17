ecd426d85a56a359204d3322cca6559302b4d07e
elijah-villanu <elijahred9@outlook.com>
2025-11-16 23:35:54 -0800

    Flip Tag Flag
    Flipped the condition logic in tags (tagOn now actually means the tag is on)

--- plotify/src/components_screens/components/Tag.jsx
+++ plotify/src/components_screens/components/Tag.jsx
@@ -2,16 +2,20 @@ import { Plus, Check } from "lucide-react";
 
 export default function Tag(props) {
   return (
-    <button type="button" className="cursor-pointer" onClick={() => props.click(props.tagKey)}>
+    <button
+      type="button"
+      className="cursor-pointer"
+      onClick={() => props.click(props.tagKey)}
+    >
       {props.tagOn ? (
-        <div className="flex gap-[0.25rem] bg-custom-gray py-[0.5rem] px-[0.75rem] rounded-lg hover:bg-gray-300">
-          {props.name}
-          <Plus />
-        </div>
-      ) : (
         <div className="flex gap-[0.25rem] bg-custom-light-blue py-[0.5rem] px-[0.75rem] rounded-lg hover:bg-custom-blue">
           {props.name}
           <Check />
+        </div>
+      ) : (
+        <div className="flex gap-[0.25rem] bg-custom-gray py-[0.5rem] px-[0.75rem] rounded-lg hover:bg-gray-300">
+          {props.name}
+          <Plus />
         </div>
       )}
     </button>
